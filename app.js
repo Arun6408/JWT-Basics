ea680fa5301c6aaddf8908cb15de78346d405629
Ch Arun <challaarunkumar007@gmail.com>
2024-09-08 13:23:24 +0530

    final commit

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use('/api/v1',mainRouter);
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8081;
 
 const start = async () => {
   try {
